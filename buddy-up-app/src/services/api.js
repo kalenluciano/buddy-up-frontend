59ad44c9159b779274d47919a1e4e1fc9a1b31b6
shondriane <shondriane.wise@yahoo.com>
2022-11-14 14:32:46 -0500

    created services folder with api anauth files

---
+++ buddy-up-app/src/services/api.js
@@ -0,0 +1,19 @@
+import Axios from 'axios'
+
+
+export const BASE_URL = "http://buddy-end-backup.herokuapp.com"
+
+const Client = Axios.create ({baseURL: BASE_URL})
+
+Client.interceptors.request.use(
+    (config)=>{
+        const token = localStorage.getItem('token')
+        if(token){
+            config.headers['authorization']= `Bearer ${token}`
+        }
+        return config
+    },
+    (error)=>Promise.reject(error)
+)
+
+export default Client
