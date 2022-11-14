30d8670beec8984bb7f02eed3d92be365ab2c13f
shondriane <shondriane.wise@yahoo.com>
2022-11-14 15:08:56 -0500

    did our routes for App.js

--- buddy-up-app/src/App.js
+++ buddy-up-app/src/App.js
@@ -1,7 +1,59 @@
-import './App.css';
 
-function App() {
-	return <div className="App"></div>;
+import { Route, Routes } from 'react-router'
+import { useState, useEffect } from 'react'
+import Category from './pages/Category'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
+import Activity from './pages/Activity'
+import Home from './pages/Home'
+import './App.css'
+import { CheckSession } from './services/Auth'
+
+
+const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+  return (
+    <div className="App">
+     <header>
+      <Nav/>
+	 </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn setUser={setUser}
+          toggleAuthenticated={toggleAuthenticated}/>} />
+          <Route path="/register" element={<Register setUser={setUser}
+          toggleAuthenticated={toggleAuthenticated} />} />
+          <Route path="/activity" element={<Activity/>} />
+		  <Route path = "/category" element={<Category/>} />
+		  <Route path = "/about" element={<About/>}/>
+		  <Route path ="/profile" element={<Profile/>}/>
+
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default App
