4b228e1f89dedd63e1096f32790d7e74f5f2fad9
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-14 17:35:48 -0500

    Added info to CategoryCard

--- buddy-up-app/src/components/CategoryCard.jsx
+++ buddy-up-app/src/components/CategoryCard.jsx
@@ -1,5 +1,39 @@
-const CategoryCard = () => {
-	return <div></div>;
+import {useState} from 'react'
+import axios from 'axios';
+import { BASE_URL } from '../globals';
+import { useEffect } from 'react';
+
+const CategoryCard = ({id, name, image}) => {
+	const [allActivities, setAllActivities] = useState([])
+	const [activityNumber, setActivityNumber] = useState(0)
+	
+	const getAllActivities = async () => {
+		const response = await axios.get(`${BASE_URL}/activites`)
+		setAllActivities(response.data)
+	}
+
+	const getTotalActivityNumber = () => {
+		let total = 0
+		allActivities.forEach((activity)=>{
+			if (activity.categoryId === id) {
+				total += 1
+			}
+		})
+		setActivityNumber(total)
+	}
+
+	useEffect(()=>{
+		getAllActivities()
+		getTotalActivityNumber()
+	}, [])
+
+	return (
+		<div>
+			<img src={image} alt={name}/>
+			<h4>{name}</h4>
+			<p>{activityNumber}</p>
+		</div>
+	);
 };
 
 export default CategoryCard;
