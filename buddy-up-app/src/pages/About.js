09c2896bf0abf75dbeb99cc5c2f41d306ffd9c81
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-14 15:55:51 -0500

    Added cookie cutter templates for components and fleshed out About page

---
+++ buddy-up-app/src/pages/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+	return (
+		<div>
+			<p>
+				BuddyUp allows you to select an activity and match with a buddy
+				of your choice.
+			</p>
+		</div>
+	);
+};
+
+export default About;
