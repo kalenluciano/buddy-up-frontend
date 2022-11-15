996a593a9da0423db7e7f3982402cf1d842cac6d
shondriane <shondriane.wise@yahoo.com>
2022-11-15 10:25:16 -0500

    fixing merging issue

---
+++ buddy-up-app/src/components/SignUp.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const SignUp = ({}) => {
+
+
+return(
+    <div>
+        <h4>If you would like to match with a buddy, click the button below</h4>
+        <Link to={'/register'}>BuddyUp</Link>
+    </div>
+    
+)
+} 
+export default SignUp
--- buddy-up-app/src/pages/Activity.js
+++ buddy-up-app/src/pages/Activity.js
@@ -1,8 +1,45 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+import BuddyList from '../components/BuddyList'
+import BuddySwipe from '../components/BuddySwipe'
+import SignUp from '../components/SignUp'
 
-const Activity = () => {
-	return <div>
-		
-	</div>;
-};
+const Activity = ({ user, authenticated }) => {
+  let { activity_id } = useParams()
+  const [selectActivity, setSelectActivity] = useState({})
 
-export default Activity;
+  const activity = async () => {
+    const response = await axios.get(`${BASE_URL}/activities/${activity_id}`)
+    console.log(response.data)
+    setSelectActivity(response.data)
+  }
+
+  useEffect(() => {
+    activity()
+  }, [])
+
+  return (
+    <div>
+      <div>
+        <h1>{selectActivity.name}</h1>
+      </div>
+      <h2>{selectActivity.date}</h2>
+      <p>{selectActivity.description}</p>
+      <h4>
+        {selectActivity.streetAddress}
+        {selectActivity.streetAddress2}
+        {selectActivity.city}
+        {selectActivity.state}
+        {selectActivity.zipCode}
+        {selectActivity.country}
+      </h4>
+      {authenticated && user ? <BuddySwipe /> : <SignUp />}
+      {authenticated && user ? <BuddyList /> : null}
+    </div>
+  )
+}
+
+export default Activity
