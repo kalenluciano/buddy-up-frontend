c226068e151de59d31a1a4fe43e4c7defb329264
shondriane <shondriane.wise@yahoo.com>
2022-11-15 13:33:22 -0500

    fix relaod on registration and setting up sign in

--- buddy-up-app/src/pages/SignIn.js
+++ buddy-up-app/src/pages/SignIn.js
@@ -1,7 +1,69 @@
-const SignIn = () => {
-	return <div>
-		
-	</div>;
-};
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import { SignInUser } from '../services/Auth'
 
-export default SignIn;
+const SignIn = ({toggleAuthenticated,setUser}) => {
+  const navigate = useNavigate()
+  const [formValues, setFormValues] = useState({ email: '', password: '' })
+  const [auth,setAuth]= useState(true)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+	
+    if (formValues.email ==='' || formValues.password ==='') {
+      setAuth(false)
+      console.log(formValues.email, formValues.password)
+    } 
+	else{
+
+    const payload = await SignInUser(formValues)
+    setFormValues({email: '', password: ''})
+    setUser(payload)
+    toggleAuthenticated(true)
+    navigate('/activities')
+	}
+  }
+
+  return (
+    <div >
+    
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button >
+            Sign In
+          </button>
+        </form>
+		{!auth && (
+        <div>
+          <h3>Provide your email and password</h3>
+        </div>
+      )}
+      </div>
+    
+  )
+}
+export default SignIn
