9f2ed588d70f932f61126791ac210e2dd46be6fd
shondriane <shondriane.wise@yahoo.com>
2022-11-15 14:22:59 -0500

    fixed global url for auth

--- buddy-up-app/src/globals.js
+++ buddy-up-app/src/globals.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://buddy-up-backend.herokuapp.com/api';
+export const BASE_URL = 'https://buddy-up-backend.herokuapp.com/api'
+//export const BASE_URL='http://localhost:3001/api'
