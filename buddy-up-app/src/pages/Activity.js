3b46b4be91a764b2cdaf3d614e447031aab40ba7
Aekangi Patel <aekangi91@gmail.com>
2022-11-15 10:01:49 -0500

    activity page has activities(no buddies yet) and total category activity number visible

--- buddy-up-app/src/pages/Activity.js
+++ buddy-up-app/src/pages/Activity.js
@@ -1,6 +1,40 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
 
 const Activity = () => {
-	return <div></div>;
-};
+  let { activity_id } = useParams()
+  const [selectActivity, setSelectActivity] = useState({})
 
-export default Activity;
+  const activity = async () => {
+    const response = await axios.get(`${BASE_URL}/activities/${activity_id}`)
+    console.log(response.data)
+    setSelectActivity(response.data)
+  }
+
+  useEffect(() => {
+    activity()
+  }, [])
+
+  return (
+    <div>
+      <div>
+        <h1>{selectActivity.name}</h1>
+      </div>
+      <h2>{selectActivity.date}</h2>
+      <p>{selectActivity.description}</p>
+      <h4>
+        {selectActivity.streetAddress}
+        {selectActivity.streetAddress2}
+        {selectActivity.city}
+        {selectActivity.state}
+        {selectActivity.zipCode}
+        {selectActivity.country}
+      </h4>
+    </div>
+  )
+}
+
+export default Activity
