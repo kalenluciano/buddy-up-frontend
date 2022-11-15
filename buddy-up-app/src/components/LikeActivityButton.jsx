a84711331678a1bf2c70d8622a37254313b03c6d
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 16:57:06 -0500

    Created like button functionality to add and drop userActivity from backend

--- buddy-up-app/src/components/LikeActivityButton.jsx
+++ buddy-up-app/src/components/LikeActivityButton.jsx
@@ -1,11 +1,27 @@
-const LikeActivityButton = ({likedActivity, toggleLikedActivity}) => {
-    const handleClick = () => {
+import axios from "axios"
+import { BASE_URL } from "../globals"
+
+const LikeActivityButton = ({likedActivity, toggleLikedActivity, userId, selectActivityId, setUserActivityList, userActivityList}) => {
+    const handleLikeClick = async () => {
+        await axios.post(`${BASE_URL}/user-activities/user/${userId}/activity/${selectActivityId}`)
+        toggleLikedActivity(!likedActivity)
+    }
+
+    const handleDropClick = async () => {
+        // Need to get userActivity PK, working on backend controller to do this
+        const userActivity = userActivityList.filter(userActivity => userActivity.id === selectActivityId)
+        if (userActivity[0]) {
+            await axios.delete(`${BASE_URL}/user-activities/${userActivity[0].id}`)
+        }
+        const response = await axios.get(`${BASE_URL}/user-activities/user/${userId}`)
+        setUserActivityList(response.data[0].created_list)
         toggleLikedActivity(!likedActivity)
     }
 
     return (
         <div>
-            <button onClick={handleClick}>{likedActivity ? "Drop Activity" : "Like Activity"}</button>
+            {!likedActivity && <button onClick={handleLikeClick}>Like Activity</button>}
+            {likedActivity && <button onClick={handleDropClick}>Drop Activity</button>}
         </div>
     )
 }
