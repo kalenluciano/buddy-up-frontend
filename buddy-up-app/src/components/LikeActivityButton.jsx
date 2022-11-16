240503ba0b4d3224e0752c7286ff05de00ff1205
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 19:47:03 -0500

    Finished activity like button functionality

--- buddy-up-app/src/components/LikeActivityButton.jsx
+++ buddy-up-app/src/components/LikeActivityButton.jsx
@@ -2,19 +2,25 @@ import axios from "axios"
 import { BASE_URL } from "../globals"
 
 const LikeActivityButton = ({likedActivity, toggleLikedActivity, userId, selectActivityId, setUserActivityList, userActivityList}) => {
+    const updateUserActivityList = async () => {
+        const response = await axios.get(`${BASE_URL}/user-activities/user/${userId}`)
+        setUserActivityList(response.data[0].created_list)
+    }
+    
     const handleLikeClick = async () => {
-        await axios.post(`${BASE_URL}/user-activities/user/${userId}/activity/${selectActivityId}`)
+        const response = await axios.post(`${BASE_URL}/user-activities/user/${userId}/activity/${selectActivityId}`)
+        updateUserActivityList()
         toggleLikedActivity(!likedActivity)
     }
 
     const handleDropClick = async () => {
-        // Need to get userActivity PK, working on backend controller to do this
-        const userActivity = userActivityList.filter(userActivity => userActivity.id === selectActivityId)
+        const userActivities = await axios.get(`${BASE_URL}/user-activities/pk`)
+        const userActivitiesList = userActivities.data
+        const userActivity = userActivitiesList.filter(userActivity => userActivity.activityListedId === selectActivityId && userActivity.userCreatedId === userId)
         if (userActivity[0]) {
             await axios.delete(`${BASE_URL}/user-activities/${userActivity[0].id}`)
         }
-        const response = await axios.get(`${BASE_URL}/user-activities/user/${userId}`)
-        setUserActivityList(response.data[0].created_list)
+        updateUserActivityList()
         toggleLikedActivity(!likedActivity)
     }
 
