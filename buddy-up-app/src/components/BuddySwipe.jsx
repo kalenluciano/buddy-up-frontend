9af4f09d7130a79abf5802a98d3fa84584a7fd8b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 22:11:41 -0500

    Added random buddy selection and toggled no more buddies

--- buddy-up-app/src/components/BuddySwipe.jsx
+++ buddy-up-app/src/components/BuddySwipe.jsx
@@ -2,27 +2,48 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { BASE_URL } from "../globals";
 import UserCard from "./UserCard";
-import { Link } from "react-router-dom";
 
 const BuddySwipe = ({user, selectActivity}) => {
-	const [allUserActivitiesList, setAllUserActivitiesList] = useState([])
+	const [poolOfBuddies, setPoolOfBuddies] = useState([])
+	const [potentialBuddy, setPotentialBuddy] = useState({})
+	const [noMorePotentialBuddies, toggleNoMorePotentialBuddies] = useState(false)
 	
 	const getAllUserActivities = async () => {
 		const response = await axios.get(`${BASE_URL}/user-activities/activity/${selectActivity.id}`)
 		const userActivitiesListFiltered = response.data[0].activities_user_list.filter(userActivity => userActivity.id !== user.id)
-		setAllUserActivitiesList(userActivitiesListFiltered)
+		setPoolOfBuddies(userActivitiesListFiltered)
+	}
+
+	const getRandomPotentialBuddy = () => {
+		const randomBuddy = poolOfBuddies[Math.floor(Math.random() * poolOfBuddies.length)]
+		setPotentialBuddy(randomBuddy)
+	}
+
+	const getNextUser = () => {
+		console.log('Next')
+	}
+
+	const addUserToBuddyList = () => {
+		console.log("Added")
 	}
 
 	useEffect(() => {
 		getAllUserActivities()
 	}, [])
 
+	useEffect(() => {
+		if (poolOfBuddies.length > 0) {
+			getRandomPotentialBuddy()
+			toggleNoMorePotentialBuddies(false)
+		} else {
+			toggleNoMorePotentialBuddies(true)
+		}
+	}, [poolOfBuddies])
+
 	return (
 		<div>
 			<h1>Buddy Swipe</h1>
-			{allUserActivitiesList.map((userBuddy) => (
-				<UserCard key={userBuddy.id} userBuddy={userBuddy}/>
-			))}
+			{noMorePotentialBuddies ? <h3>Waiting for More Buddies...</h3> : <UserCard userBuddy={potentialBuddy} getNextUser={getNextUser} addUserToBuddyList={addUserToBuddyList} />}
 		</div>
 		);
 };
--- buddy-up-app/src/components/UserCard.jsx
+++ buddy-up-app/src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import ProfileInfo from "./ProfileInfo"
 
-const UserCard = ({userBuddy}) => {
+const UserCard = ({userBuddy, getNextUser, addUserToBuddyList}) => {
     const [showMore, toggleShowMore] = useState(false)
 
     const setShowMore = () => {
@@ -10,10 +10,12 @@ const UserCard = ({userBuddy}) => {
 
     return (
         <div>
-            {showMore ? <ProfileInfo user={userBuddy} setShowMore={setShowMore}/> : <img src={userBuddy.profilePicture ? userBuddy.profilePicture : null} alt={userBuddy.firstName + userBuddy.lastName}/>}
+            {showMore ? <ProfileInfo user={userBuddy} setShowMore={setShowMore}/> : <img src={userBuddy.profilePicture ? userBuddy.profilePicture : null} alt={`${userBuddy.firstName} ${userBuddy.lastName}`}/>}
             <h3>{userBuddy.firstName} {userBuddy.lastName}</h3>
             <p>{userBuddy.age}</p>
+            <button onClick={getNextUser}>Next</button>
             {!showMore && <button onClick={setShowMore}>Show More</button>}
+            <button onClick={addUserToBuddyList}>BuddyUp</button>
         </div>
     )
 }
