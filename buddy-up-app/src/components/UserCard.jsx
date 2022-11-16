45794e1bf268aab918b60e8fb6507424e8829480
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-15 21:21:02 -0500

    Added profile info component and toggle show more button

--- buddy-up-app/src/components/UserCard.jsx
+++ buddy-up-app/src/components/UserCard.jsx
@@ -1,8 +1,19 @@
-const UserCard = ({userActivity}) => {
+import { useState } from "react"
+import ProfileInfo from "./ProfileInfo"
+
+const UserCard = ({userBuddy}) => {
+    const [showMore, toggleShowMore] = useState(false)
+
+    const setShowMore = () => {
+        toggleShowMore(!showMore)
+    }
 
     return (
         <div>
-
+            {showMore ? <ProfileInfo user={userBuddy} setShowMore={setShowMore}/> : <img src={userBuddy.profilePicture ? userBuddy.profilePicture : null} alt={userBuddy.firstName + userBuddy.lastName}/>}
+            <h3>{userBuddy.firstName} {userBuddy.lastName}</h3>
+            <p>{userBuddy.age}</p>
+            {!showMore && <button onClick={setShowMore}>Show More</button>}
         </div>
     )
 }
