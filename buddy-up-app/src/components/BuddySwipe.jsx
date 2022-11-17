7f3a65f58aadd86c3d4b47fddbc596a377eac148
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-11-16 22:27:39 -0500

    Added remove potential buddy functionality

--- buddy-up-app/src/components/BuddySwipe.jsx
+++ buddy-up-app/src/components/BuddySwipe.jsx
@@ -2,15 +2,26 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { BASE_URL } from "../globals";
 import UserCard from "./UserCard";
+import Client from '../services/api'
 
 const BuddySwipe = ({user, selectActivity}) => {
 	const [poolOfBuddies, setPoolOfBuddies] = useState([])
 	const [potentialBuddy, setPotentialBuddy] = useState({})
 	const [noMorePotentialBuddies, toggleNoMorePotentialBuddies] = useState(false)
-	
-	const getAllUserActivities = async () => {
+	const [clickNext, toggleClickNext] = useState(false)
+	const [clickBuddyUp, toggleClickBuddyUp] = useState(false)
+	const [userId, setUserId] = useState(null)
+
+	const getUserId = () => {
+		const userId = parseInt(user.id)
+		setUserId(userId)
+	}
+
+	const getPoolOfBuddies = async () => {
 		const response = await axios.get(`${BASE_URL}/user-activities/activity/${selectActivity.id}`)
 		const userActivitiesListFiltered = response.data[0].activities_user_list.filter(userActivity => userActivity.id !== user.id)
+		// remove buddies that the user already liked FOR THIS ACTIVITY
+		// removed buddies that the user already rejected FOR THIS ACTIVITY
 		setPoolOfBuddies(userActivitiesListFiltered)
 	}
 
@@ -21,11 +32,15 @@ const BuddySwipe = ({user, selectActivity}) => {
 
 	const removePotentialBuddyFromPool = async () => {
 		const potentialBuddyId = potentialBuddy.id
-		
+		const userRejectedBuddy = await Client.post(`${BASE_URL}/next-buddies/user/${userId}/rejected-buddy/${potentialBuddyId}`)
+		const userRejectBuddyId = userRejectedBuddy.data[0][0].id
+		const userRejectedBuddyActivity = await Client.post(`${BASE_URL}/next-buddy-activities/user-rejected-buddy/${userRejectBuddyId}/activity/${selectActivity.id}`)
+		console.log(userRejectedBuddyActivity)
 	}
 
 	const getNextUser = () => {
-		console.log('Next')
+		removePotentialBuddyFromPool()
+		toggleClickNext(!clickNext)
 	}
 
 	const addUserToBuddyList = () => {
@@ -33,8 +48,9 @@ const BuddySwipe = ({user, selectActivity}) => {
 	}
 
 	useEffect(() => {
-		getAllUserActivities()
-	}, [])
+		getUserId()
+		getPoolOfBuddies()
+	}, [user, clickNext])
 
 	useEffect(() => {
 		if (poolOfBuddies.length > 0) {
